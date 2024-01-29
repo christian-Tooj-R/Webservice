cc7b5e410ebf39d0f55c3890845d02f3819fa5a7
christian-Tooj-R <118376824+christian-Tooj-R@users.noreply.github.com>
2024-01-29 08:27:39 +0300

    lastmodif

--- src/main/java/com/spring/connexion/Connect.java
+++ src/main/java/com/spring/connexion/Connect.java
@@ -15,11 +15,11 @@ import java.sql.DriverManager;
 public class Connect {
     
     public Connection getConnection()throws Exception{
-    //    Class.forName("org.postgresql.Driver");
-    //    String connect = "jdbc:postgresql://dpg-cmfemneg1b2c73cl3nm0-a.oregon-postgres.render.com:5432/clouds5?user=clouds5_user&password=kaYVbGZ75Pd5bC73FuMr4WXcqz8KLm1W";
-    //    Connection con = DriverManager.getConnection(connect);
-            
-        ConnectToDatabase conn = new ConnectToDatabase("postgres","", "vehicule");
-        return conn.getConnectionPostgres();
+        Class.forName("org.postgresql.Driver");
+        String connect = "jdbc:postgresql://dpg-cmfemneg1b2c73cl3nm0-a.oregon-postgres.render.com:5432/clouds5?user=clouds5_user&password=kaYVbGZ75Pd5bC73FuMr4WXcqz8KLm1W";
+        Connection con = DriverManager.getConnection(connect);
+        return con;     
+    //    ConnectToDatabase conn = new ConnectToDatabase("postgres","", "vehicule");
+    //    return conn.getConnectionPostgres();
     }
 }
