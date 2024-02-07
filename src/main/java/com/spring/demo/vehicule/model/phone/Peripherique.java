674a944ad1d67965b7e1099ff40727d853404a78
christian-Tooj-R <118376824+christian-Tooj-R@users.noreply.github.com>
2024-02-07 13:27:05 +0300

    modif entity peripherique

--- src/main/java/com/spring/demo/vehicule/model/phone/Peripherique.java
+++ src/main/java/com/spring/demo/vehicule/model/phone/Peripherique.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "token")
+@Table(name = "peripherique")
 public class Peripherique {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
